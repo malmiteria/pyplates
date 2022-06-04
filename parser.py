5f7a342d668d858e002089f03cb9360f84c4bcf8
malmiteria <malmiteria@gmail.com>
2022-06-04 23:33:29 +0200

    handles elifs

--- parser.py
+++ parser.py
@@ -22,9 +22,9 @@ if {ifexpression}:
 {elifs}{else_block}
 """
 
-def make_python_if(if_expr, if_block, elifs_expr="", elifs_blocks="", else_block=""):
+def make_python_if(if_expr, if_block, elifs_expr="", elifs_block="", else_block=""):
     if elifs_expr:
-        elifs = "".join(ELIF.format(elifexpression=elif_expr, elifblock=elif_block) for elif_expr, elif_block in zip(elifs_expr, elifs_blocks))
+        elifs = "".join(ELIF.format(elifexpression=elif_expr, elifblock=elif_block) for elif_expr, elif_block in zip(elifs_expr, elifs_block))
     else:
         elifs = ""
     else_block = ELSE.format(elseblock=else_block)
@@ -43,14 +43,25 @@ def render(file_content, *args, **kwargs):
         )
         file_content = re.sub(for_block, for_parsed, file_content)
 
-    if_block = "\{\% if (?P<ifexpression>[^(\%\})]*) \%\}(?P<ifblock>[^(\{\%)]*)(\{\% elif (?P<elifexpression>[^(\%\})]*) \%\}(?P<elifblock>[^(\%\})]*))*(\{\% else \%\}(?P<elseblock>[^(\%\})]*))?\{\% endif \%\}"
+    elif_pattern = "\{\% elif (?P<elifexpression>[^(\%\})]*) \%\}(?P<elifblock>[^(\{\%)]*)"
+    elifs_pattern = f"(?P<elifs>({elif_pattern})*)"
+    if_block = "\{\% if (?P<ifexpression>[^(\%\})]*) \%\}(?P<ifblock>[^(\{\%)]*)" + elifs_pattern + "(\{\% else \%\}(?P<elseblock>[^(\%\})]*))?\{\% endif \%\}"
     for res in re.finditer(if_block, file_content):
+        elif_els = res.group("elifs")
+        elifs_expr, elifs_block = "", ""
+        if elif_els:
+            elifs_expr, elifs_block = zip(*get_elifs_content(elif_pattern, elif_els))
+
         if_parsed = make_python_if(
             res.group("ifexpression"),
             res.group("ifblock"),
-            [res.group("elifexpression")],
-            [res.group("elifblock")],
+            elifs_expr = elifs_expr,
+            elifs_block = elifs_block,
             else_block=res.group("elseblock"),
         )
         file_content = re.sub(if_block, if_parsed, file_content)
     return file_content
+
+def get_elifs_content(elif_pattern, elif_els):
+    for res_elif in re.finditer(elif_pattern, elif_els):
+        yield res_elif.group("elifexpression"), res_elif.group("elifblock")
